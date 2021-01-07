6c2d1483d1bc9cf9b0a825cbbb9315dc4c13ff96
rifkihariyanto25 <hariyantorifki25@gmail.com>
2021-01-07 16:46:26 +0700

    first commit

---
+++ Myproject/Form1.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(13, 224)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(717, 225)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Location = New System.Drawing.Point(13, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(224, 30)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "NIM"
+        '
+        'Label2
+        '
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label2.Location = New System.Drawing.Point(13, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(224, 30)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "NAMA"
+        '
+        'Label3
+        '
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label3.Location = New System.Drawing.Point(12, 101)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(224, 30)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "ALAMAT"
+        '
+        'Label4
+        '
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label4.Location = New System.Drawing.Point(13, 140)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(224, 30)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "TELPON"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(270, 14)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(219, 22)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(270, 58)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(460, 22)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(270, 99)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(460, 22)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(270, 138)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(219, 22)
+        Me.TextBox4.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(270, 178)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(90, 40)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "INPUT"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(399, 178)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(90, 40)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "EDIT"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(525, 178)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(90, 40)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "HAPUS"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(642, 178)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(90, 40)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "TUTUP"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(744, 462)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+
+End Class
---
+++ Myproject/Form1.vb
@@ -0,0 +1,110 @@
+﻿Imports System.Data.Odbc
+Public Class Form1
+    Dim Conn As OdbcConnection
+    Dim Cmd As OdbcCommand
+    Dim Ds As DataSet
+    Dim Da As OdbcDataAdapter
+    Dim Rd As OdbcDataReader
+    Dim MyDB As String
+    Sub koneksi()
+        MyDB = "Driver={MySQL ODBC 3.51 Driver};Database=db_kampus;Server=localhost;uid=root"
+        Conn = New OdbcConnection(MyDB)
+        If Conn.State = ConnectionState.Closed Then Conn.Open()
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call kondisiawal()
+    End Sub
+
+    Sub kondisiawal()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        Button1.Text = "INPUT"
+        Button2.Text = "EDIT"
+        Button3.Text = "HAPUS"
+        Button4.Text = "TUTUP"
+        Call koneksi()
+        Da = New OdbcDataAdapter("Select * From tbl_mahasiswa", Conn)
+        Ds = New DataSet
+        Da.Fill(Ds, "tbl_mahasiswa")
+        DataGridView1.DataSource = Ds.Tables("tbl_mahasiswa")
+    End Sub
+   
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+       
+            If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Then
+                MsgBox("Pastikan Semua Field Terisi Semua")
+            Else
+                Call koneksi()
+                Dim InputData As String = "Insert into tbl_mahasiswa values ('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "','" & TextBox4.Text & "')"
+                Cmd = New OdbcCommand(InputData, Conn)
+                Cmd.ExecuteNonQuery()
+                MsgBox("Input Data Berhasil")
+                Call kondisiawal()
+            End If
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+       
+
+            If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Then
+                MsgBox("Pastikan Semua Field Terisi Semua")
+            Else
+                Call koneksi()
+                Dim EditData As String = "update tbl_mahasiswa set nama='" & TextBox2.Text & "',alamat='" & TextBox3.Text & "',telpon='" & TextBox4.Text & "' where nim='" & TextBox1.Text & "'"
+                Cmd = New OdbcCommand(EditData, Conn)
+                Cmd.ExecuteNonQuery()
+                MsgBox("Edit Data Berhasil")
+                Call kondisiawal()
+            End If
+
+    End Sub
+
+    Private Sub TextBox1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+        If e.KeyChar = Chr(13) Then
+            Call koneksi()
+            Cmd = New OdbcCommand("Select * From tbl_mahasiswa where nim = '" & TextBox1.Text & "'", Conn)
+            Rd = Cmd.ExecuteReader
+            Rd.Read()
+            If Rd.HasRows Then
+                TextBox2.Text = Rd.Item("nama")
+                TextBox3.Text = Rd.Item("alamat")
+                TextBox4.Text = Rd.Item("telpon")
+            Else
+                MsgBox("Data Tidak Ada")
+
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        
+
+            If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Then
+                MsgBox("Pastikan Data Yang Dihapus Terisi")
+            Else
+                Call koneksi()
+                Dim HapusData As String = "delete from tbl_mahasiswa  where nim='" & TextBox1.Text & "'"
+                Cmd = New OdbcCommand(HapusData, Conn)
+                Cmd.ExecuteNonQuery()
+                MsgBox("Hapus Data Berhasil")
+                Call kondisiawal()
+            End If
+
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If Button4.Text = "TUTUP" Then
+            End
+        Else
+            Call kondisiawal()
+        End If
+    End Sub
+End Class
